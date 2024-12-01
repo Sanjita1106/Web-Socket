4792601bd959b2b0e606fc97fc0f1d53bf5830b2
Sanjita097 <sanjita9804050268@gmail.com>
2024-12-01 12:48:08 +0545

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const{Server} =require("socket.io")
+const app = express()
+const http = require("http");
+
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:
+    {
+      origin:"https://localhost:5173",
+      methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("A user connected with ID of :",socket.id);
+    socket.on("message",(message)=>{
+        console.log("Message received:", message);
+        io.emit("message", message);
+    });
+    socket.on("close",()=>{
+        console.log("User disconnected with ID of:", socket.id)
+    });
+});
+
+const PORT = 3003;
+
+app.use("/", (req,res)=>{
+    res.send("Hello!!")
+})
+
+app.listen(PORT,()  => console.log("server running on port: ", PORT));
